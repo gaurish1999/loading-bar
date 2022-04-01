7b6d167b68a89becafd9cf48feb1b702b39a7585
Gaurish <gaurish1708.cse18@chitkara.edu.in>
2022-04-01 19:34:14 +0530

    Made it!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+
+function setNum(){
+  const x = document.getElementById('input').value;
+  // alert(x + "%");
+
+  if(x >= 0 && x <= 100){
+    const y = x + "%";
+    document.querySelector('.progress .progress-bar').innerHTML = y;
+    document.querySelector('.progress .progress-bar').setAttribute('style', 'width: ' + y)  
+
+  }
+  else alert("Oh please")
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container bg-light p-5">
+
+      <h2>Loading Bar</h2>
+
+      <div className="progress" style={{height: '40px'}}>
+        <div className="progress-bar" 
+        style={{width: '55%'}} 
+        value='30'
+        >55%</div>
+      </div>
+
+      <br/>
+
+      <input type='number' id='input' className='form-control' placeholder='Enter the percentage'></input><br/>
+      <button className='btn btn-dark w-75'
+      onClick={setNum}
+      >Enter</button>
+
     </div>
   );
 }
